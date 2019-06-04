37329bba776eeedcd6dae04ce3ff4d3de66ab96a
Paul Hahn <19797445+madp4ul@users.noreply.github.com>
2019-06-04 17:21:41 +0200

    replaced wpf view with winforms due to opentk compatibility

--- Viewer/Properties/Resources.Designer.cs
+++ Viewer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace View.Properties
+namespace Viewer.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace View.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("View.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Viewer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
