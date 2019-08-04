ee1649ff4bb2841791f96982e0de34a52f80c98c
Paul Hahn <19797445+madp4ul@users.noreply.github.com>
2019-08-04 15:50:32 +0200

    added more point cloud files and modified off reader

--- GeometricAlgorithms.FileProcessing/OFFReader.cs
+++ GeometricAlgorithms.FileProcessing/OFFReader.cs
@@ -24,28 +24,66 @@ namespace GeometricAlgorithms.FileProcessing
 
             if (lines[0] == "OFF")
             {
-                string[] itemCounts = lines[1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                int pointCount = int.Parse(itemCounts[0]);
-
-                GenericVertex[] points = new GenericVertex[pointCount];
-                for (int i = 0; i < pointCount; i++)
-                {
-                    string[] vertexDimensions = lines[i + 2].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    Vector3 vertex = new Vector3(
-                        float.Parse(vertexDimensions[0], CultureInfo.InvariantCulture),
-                        float.Parse(vertexDimensions[1], CultureInfo.InvariantCulture),
-                        float.Parse(vertexDimensions[2], CultureInfo.InvariantCulture));
-
-                    points[i] = new GenericVertex(vertex);
-                }
-
-                return points;
+                return ReadFile(lines, ProcessOFFVertexLine);
+            }
+            else if (lines[0] == "NOFF")
+            {
+                return ReadFile(lines, ProcessNOFFVertexLine);
             }
             else
             {
                 throw new ArgumentException($"{offFilePath} is not a valid off file");
             }
+        }
+
+        private GenericVertex[] ReadFile(string[] lines, Func<string[], GenericVertex> processLine)
+        {
+            string[] itemCounts = SplitLine(lines[1]);
+            int pointCount = int.Parse(itemCounts[0]);
+            int faceCount = int.Parse(itemCounts[1]);
+
+            GenericVertex[] points = new GenericVertex[pointCount];
+            for (int i = 0; i < pointCount; i++)
+            {
+                string[] lineData = SplitLine(lines[i + 2]);
+                points[i] = processLine(lineData);
+            }
+
+            return points;
+        }
+
+        private GenericVertex ProcessOFFVertexLine(string[] lineData)
+        {
+            Vector3 vertex = new Vector3(
+                ParseFloat(lineData[0]),
+                ParseFloat(lineData[1]),
+                ParseFloat(lineData[2]));
+
+            return new GenericVertex(vertex);
+        }
+
+        private GenericVertex ProcessNOFFVertexLine(string[] lineData)
+        {
+            GenericVertex vertex = ProcessOFFVertexLine(lineData);
+
+            Vector3 normal = new Vector3(
+                ParseFloat(lineData[3]),
+                ParseFloat(lineData[4]),
+                ParseFloat(lineData[5]));
+
+            vertex.Data = normal;
+
+            return vertex;
+        }
+
+        private float ParseFloat(string floatString)
+        {
+            return float.Parse(floatString, CultureInfo.InvariantCulture);
+        }
+
+        private string[] SplitLine(string line)
+        {
+            return line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
