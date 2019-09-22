bf018e8abe488db763e5148b39de89ddca577a18
Paul Hahn <19797445+madp4ul@users.noreply.github.com>
2019-09-22 02:50:26 +0200

    added creation of missing insides

--- GeometricAlgorithms/ImplicitSurfaces/MarchingOctree2/CubeInsides.cs
+++ GeometricAlgorithms/ImplicitSurfaces/MarchingOctree2/CubeInsides.cs
@@ -27,8 +27,85 @@ namespace GeometricAlgorithms.ImplicitSurfaces.MarchingOctree2
                 side.CreateChildren();
             }
 
+            var insideEdges = new CubeInsideEdges(implicitSurface, outsideContainer);
+
+            for (int i = 0; i < (int)Dimension.Count; i++)
+            {
+                Dimension current = (Dimension)i;
+                var edgeDirectionsFromCubeCenter = Dimensions.All.Where(d => d != current).ToArray();
+
+                var sideEdges00 = new SideOutsideEdges(current);
+                sideEdges00[0, 0] = outsideContainer[new SideOrientation(edgeDirectionsFromCubeCenter[0], false)]
+                    .Children[0, 0]
+                    .Edges[edgeDirectionsFromCubeCenter[0], true];
+
+                //iterate over all side indices
+                for (int a = 0; a < 2; a++)
+                {
+                    for (int b = 0; b < 2; b++)
+                    {
+                        var sideEdges = new SideOutsideEdges(current);
+
+                        for (int edgeDimension = 0; edgeDimension < 2; edgeDimension++)
+                        {
+                            for (int edgeDirection = 0; edgeDirection < 2; edgeDirection++)
+                            {
+                                bool isEdgeInside = (edgeDimension == 0 && a != edgeDimension)
+                                    || (edgeDimension == 1 && b != edgeDimension);
+
+                                Edge selectedEdge;
+
+                                if (isEdgeInside)
+                                {
+                                    Dimension dimIndexOfInsideEdge = edgeDirectionsFromCubeCenter[edgeDimension == 0 ? 1 : 0];
+                                    bool directionIndexOfInsideEdge = (edgeDimension == 0 ? a : b) == 0;
+
+                                    selectedEdge = insideEdges[dimIndexOfInsideEdge, directionIndexOfInsideEdge];
+                                }
+                                else
+                                {
+                                    SideOrientation outsideOrientation = new SideOrientation(
+                                        dimension: edgeDirectionsFromCubeCenter[edgeDimension],
+                                        isMax: edgeDirection == 1);
+                                    Side outsideToLookIn = outsideContainer[outsideOrientation];
+
+                                    //TODO improve
+
+                                    var dimensionInOutside = outsideToLookIn.Dimensions.SideAxis
+                                         .Select((dimension, index) => new { dimension, index })
+                                         .First(d => sideEdges.Dimensions.SideAxis.Contains(d.dimension));
+
+                                    var dimensionInCreatedSide = sideEdges.Dimensions.SideAxis
+                                        .Select((dimension, index) => new { dimension, index })
+                                        .First(d => sideEdges.Dimensions.SideAxis.Contains(d.dimension));
+
+                                    int direction = dimensionInCreatedSide.index == 0 ? a : b;
+
+                                    selectedEdge = GetCrossEdge(outsideToLookIn, dimensionInOutside.index, direction);
+                                }
+
+                                sideEdges[edgeDimension, edgeDirection] = selectedEdge;
+                            }
+                        }
+                    }
+                }
+
+            }
+
             //Todo create sides from available data and create all necessary edges and fv.
 
+        }
+
+        private Edge GetCrossEdge(Side side, int dimensionIndex, int directionIndex)
+        {
+            if (dimensionIndex == 0)
+            {
+                return side.Children[directionIndex, 0].Edges[1, 1];
+            }
+            else
+            {
+                return side.Children[0, directionIndex].Edges[0, 1];
+            }
         }
 
         public Side this[SideOrientation orientation, OctreeOffset childOffset]
@@ -47,9 +124,14 @@ namespace GeometricAlgorithms.ImplicitSurfaces.MarchingOctree2
                 throw new InvalidOperationException("The given orientation from the offset is not on the inside");
             }
 
-            int index = (int)orientationAxis * 4;
-
             SideOffset childSideOffset = childOffset.ExcludeDimension(orientationAxis);
+
+            return ToIndex(orientationAxis, childSideOffset);
+        }
+
+        private int ToIndex(Dimension orientationAxis, SideOffset childSideOffset)
+        {
+            int index = (int)orientationAxis * 4;
 
             index += childSideOffset.MinimumDimensionValue * 2;
             index += childSideOffset.MaximumDimensionValue;
