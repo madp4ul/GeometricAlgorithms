a65d3676a4d7edabcdfe6e8aeee07737964c9782
Paul Hahn <19797445+madp4ul@users.noreply.github.com>
2019-12-16 16:31:42 +0100

    bugfix?

--- Viewer/Utilities/FormProgressUpdater.cs
+++ Viewer/Utilities/FormProgressUpdater.cs
@@ -25,20 +25,26 @@ namespace GeometricAlgorithms.Viewer.Utilities
 
         public void UpdateStatus(int percentDone, string message = null)
         {
-            //Scaling from 100 to 10000
-            int scaledProgress = percentDone * 100;
-
-            ProgressBar.Value = scaledProgress;
-
-            //Due to bug with with windows animation we have to go backwards one step at the end
-            //Otherwise the progress bar movement would just stop without reaching the end
-            //https://stackoverflow.com/questions/23476932/progress-bar-does-not-reach-100
-            if (ProgressBar.Maximum == scaledProgress)
+            if (!ProgressBar.IsDisposed)
             {
-                ProgressBar.Value = scaledProgress - 1;
+                //Scaling from 100 to 10000
+                int scaledProgress = percentDone * 100;
+
+                ProgressBar.Value = scaledProgress;
+
+                //Due to bug with with windows animation we have to go backwards one step at the end
+                //Otherwise the progress bar movement would just stop without reaching the end
+                //https://stackoverflow.com/questions/23476932/progress-bar-does-not-reach-100
+                if (ProgressBar.Maximum == scaledProgress)
+                {
+                    ProgressBar.Value = scaledProgress - 1;
+                }
             }
 
-            ProgressMessageLabel.Text = message;
+            if (!ProgressMessageLabel.IsDisposed)
+            {
+                ProgressMessageLabel.Text = message;
+            }
         }
     }
 }
